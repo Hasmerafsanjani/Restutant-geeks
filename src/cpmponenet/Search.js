9da8e2447ffc8cf3be79733b1f37d2579c00cdcd
Hasme Rafsanjani <hasmerafsanjani@gmail.com>
2023-11-27 12:37:56 +0530

    some modified

--- src/cpmponenet/Search.js
+++ src/cpmponenet/Search.js
@@ -4,13 +4,13 @@ function Search() {
   return (
     <div className='flex justify-between m-5'>
         <input
-        className='shadow-[0_35px_60px_-15px_rgba(0,0,0,1)] p-2  bg-slate-200'
+        className=' p-2 rounded-2xl  bg-slate-200'
          type='text'
         placeholder='Search Rasturant'
         ></input>
-        <div className='p-2 bg-slate-200'>
+        <div className='p-2 rounded-2xl bg-slate-200'>
             <label>Search By Rating :</label>
-            <input className='w-16  bg-slate-200' type='number'></input>
+            <input className='w-16  bg-gray-400-200 p-1' type='number'></input>
         </div>
     </div>
   )
